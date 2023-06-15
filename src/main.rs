58d2567e720d4209af3b05bb44dbedc58a1155c8
Levi Noecker <levi.noecker@gmail.com>
2023-06-15 07:35:40 -0500

    Chap 3.3 complete

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-fn foo() -> usize {
-    1
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
 }
 
-#[test]
-fn test_foo() {
-    assert_eq!(foo(), 1);
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
-fn main() {
-    println!("Hello, world!");
-    println!("testing");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.01:8000")?
+    .run()
+    .await
 }
