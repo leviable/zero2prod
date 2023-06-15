0fdf5ffdd0a6bde64dcaed6260ea9746fd90b49a
Levi Noecker <levi.noecker@gmail.com>
2023-06-15 16:37:16 -0500

    3.8.5.3 Connected integration test to postgres docker image

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -28,6 +29,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -37,7 +43,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .body(body)
         .send()
         .await
-        .expect("Failed tp executre request.");
+        .expect("Failed to execute request.");
 
     println!("{:?}", &response);
     assert_eq!(200, response.status().as_u16());
