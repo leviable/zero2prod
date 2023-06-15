ecd8ec9c4dc98b77dd48e082d919d0024c276537
Levi Noecker <levi.noecker@gmail.com>
2023-06-15 09:37:03 -0500

    3.7.3 complete

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.01:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+    run(listener)?.await
 }
