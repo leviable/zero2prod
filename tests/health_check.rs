523f0e3e3af3c0bd1e39ec750483833fd41cacee
Levi Noecker <levi.noecker@gmail.com>
2023-06-17 07:24:59 -0500

    3.8.5.4 Working as intended

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -31,7 +31,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("failed to connect to Postgres.");
     let client = reqwest::Client::new();
@@ -47,6 +47,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     println!("{:?}", &response);
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
