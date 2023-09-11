521b0db71638bd018d6cdbec676edb96388d48d3
Levi Noecker <levi.noecker@gmail.com>
2023-09-11 07:20:53 -0500

    Testing github actions caching performance

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -46,6 +46,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
         .expect("Failed to connect to Postgres.");
+
     connection
         .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
